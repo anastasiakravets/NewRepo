1d054614e26bbc68598fd2fd68d1c4d195012c2e
Anastasia Kravets <583012a5230@gmail.com>
2016-03-20 14:22:52 +0200

    first step

---
+++ lab7op/lab7op/lab7op.cpp
@@ -0,0 +1,18 @@
+/* lab7op.cpp : 
+Із заданого рядка символів вилучити слова, довжина яких менша, за вказану користувачем.
+*/
+
+#include "stdafx.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	_getch();
+    return 0;
+}
+
