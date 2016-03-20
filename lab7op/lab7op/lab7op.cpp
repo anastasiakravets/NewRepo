eef6bd318741ebf95a192ef08b32f74628622277
Anastasia Kravets <583012a5230@gmail.com>
2016-03-20 20:05:34 +0200

    version 1

--- lab7op/lab7op/lab7op.cpp
+++ lab7op/lab7op/lab7op.cpp
@@ -7,25 +7,56 @@
 #include<stdlib.h>
 #include<conio.h>
 #include<iostream>
+#include <ctype.h>
+#include <string.h>
 
 using namespace std;
-int n = 0;
 
+int n = 0,
+	i=0;
+char arr[100], 
+	*p,
+	arr1[30];
 
 void input(char*);
+void detect(char*);
+void output();
 
 int main()
 {
-
+	input(arr);
+	detect(arr);
+	output();
 	_getch();
     return 0;
 }
 
 void input(char* str) {
 	printf("Enter your string:\n");
+	fgets(arr, 100, stdin);
 	do
 	{
 		printf("Enter n:\n");
-		scanf("%d", &n);
+		scanf_s("%d", &n);
 	} while (n<0);
 }
+
+void detect(char* str) {
+	p = arr;
+	while (p && *p != '\0' && *p != 10)
+	{
+		while (isspace(*p))
+			p++;
+		sscanf_s(p, "%s", arr1);
+		if (strlen(arr1)>n)
+		{
+			memmove(p, p + strlen(arr1) + 1, strlen(arr) + 2);
+		}
+		else p += strlen(arr1);
+	}
+}
+
+	void output()
+	{
+		printf("Result: %s", arr);
+	}
