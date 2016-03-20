3383f32b57e913df14926b0c4b8c8543216e4738
Anastasia Kravets <583012a5230@gmail.com>
2016-03-20 19:02:08 +0200

    input func

--- lab7op/lab7op/lab7op.cpp
+++ lab7op/lab7op/lab7op.cpp
@@ -9,10 +9,23 @@
 #include<iostream>
 
 using namespace std;
+int n = 0;
+
+
+void input(char*);
 
 int main()
 {
+
 	_getch();
     return 0;
 }
 
+void input(char* str) {
+	printf("Enter your string:\n");
+	do
+	{
+		printf("Enter n:\n");
+		scanf("%d", &n);
+	} while (n<0);
+}
